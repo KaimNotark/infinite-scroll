61119959626b6051a188419a1104f4575b775f58
KaimNotark <kaimnotark@yandex.ru>
2020-01-31 12:00:21 +0800

    done window for test

--- src/Api.js
+++ src/Api.js
@@ -1,48 +1,18 @@
 import axios from "axios";
 
 
-class ApplicantsApi {
+class RandomuserApi {
   constructor() {
-    this.baseUrl = 'http://localhost:1337/applicants'
+    this.baseUrl = 'https://randomuser.me/api/'
 
   }
 
-  async removeById(id) {
-    console.log("APIjs method 'removeById' is run. ID = " + id);
-    if (confirm("Вы действительно хотите удалить соискателя?")) {
-      try {
-        const response = await axios.delete(`${this.baseUrl}/${id}`);
-        console.log(response);
-        // this.showApplicants();
-      } catch (error) {
-        console.log(error);
-      }
-    }
-  }
-
-  async addApplicants ( addOneApplicant ) {
-    console.log("APIjs method ADD APPLICANT run.");
-
-    try {
-      await axios.post(this.baseUrl, addOneApplicant);
-      console.log("APIjs axios.post -- " + addOneApplicant);
-    } catch (error) {
-      console.log(error);
-      throw error;
-    }
-
-    // .then(response => {
-    //   console.log(response);
-    // })
-    // .catch(error => console.log(error));
-  }
-
-  async showApplicants() {
-    console.log("CLASS Function SHOW ALL APPLICANTS run.");
+  async showRandomuser() {
+    console.log("APIjs -- Function SHOW Randomuser run.");
 
     try {
       const { data } = await axios.get(this.baseUrl);
-      console.log("CLASS allApplicants -- ", data);
+      console.log("APIjs -- Randomuser = ", data);
       return data;
     } catch (error) {
       console.log(error);
@@ -50,9 +20,9 @@ class ApplicantsApi {
     }
   }
 
-  async getApplicants() {
-    return Promise.resolve(this.applicants);
+  async getRandomuser() {
+    return Promise.resolve(this.randomuser);
   }
 }
 
-export const Applicants = new ApplicantsApi();
+export const Randomuser = new RandomuserApi();
--- src/App.vue
+++ src/App.vue
@@ -4,20 +4,91 @@
       <header class="header">
         <div class="header-container">
           <img src="/images/infinity-scroll.png" alt class="header-img" />
+          <a href="https://kaimnotark.github.io/LP_DEAB/" class="header-link" target="_blank">
+            <span class="__capital">landing page</span>
+            (presentation)
+          </a>
         </div>
       </header>
+
+      <main class="main">
+        <div class="main-window">
+          <h2 class="main-window__title">window for test</h2>
+          <hr class="main-window__devider" />
+          <p class="main-window__text">Press button for load data from API</p>
+          <Button @download="showRandomuser" />
+          <Card :data-card="dataCard" />
+        </div>
+        <div class="main-window">
+          <h2 class="main-window__title">window for infinity scroll</h2>
+          <hr class="main-window__devider" />
+        </div>
+      </main>
+
+      <footer class="footer">
+        <a href="https://kaimnotark.github.io/LP_DEAB/" class="footer-link" target="_blank">
+          <span class="__capital">landing page</span>
+        </a>
+      </footer>
     </div>
   </div>
 </template>
 
 <script>
-// import HelloWorld from './components/HelloWorld.vue'
+import Button from "./components/Button.vue";
+import Card from "./components/Card.vue";
+
+import { Randomuser } from "./Api";
 
 export default {
   name: "app",
 
   components: {
-    // HelloWorld
+    Button,
+    Card
+  },
+
+  data() {
+    return {
+      wholeRandomuser: [],
+
+      dataCard: {
+        name: "",
+        mail: "",
+        imgUrl: ""
+      }
+    };
+  },
+
+  // created() {
+  // this.showApplicants();
+  // },
+
+  methods: {
+    async showRandomuser() {
+      console.log("APP -- method showRandomuser run.");
+      try {
+        this.wholeRandomuser = await Randomuser.showRandomuser();
+        console.log("APP -- wholeRandomuser = " + this.wholeRandomuser);
+        this.parsing();
+      } catch (error) {
+        console.error(error);
+      }
+    },
+
+    parsing() {
+      console.log("APP -- method parsing run.");
+      this.dataCard.name =
+        this.wholeRandomuser.results[0].name.title +
+        ". " +
+        this.wholeRandomuser.results[0].name.first +
+        " " +
+        this.wholeRandomuser.results[0].name.last;
+      console.log("APP -- name = " + this.dataCard.name);
+
+      this.dataCard.mail = this.wholeRandomuser.results[0].email;
+      this.dataCard.imgUrl = this.wholeRandomuser.results[0].picture.large;
+    }
   }
 };
 </script>
@@ -58,6 +129,8 @@ body {
   position: relative;
 
   &-container {
+    display: flex;
+    justify-content: space-between;
     padding: 15px 27px;
     background-color: $color-gray;
     box-shadow: 0px 2px 10px rgba(0, 0, 0, 0.3);
@@ -66,5 +139,117 @@ body {
   &-img {
     user-select: none;
   }
+
+  &-link {
+    color: $color-white;
+    font-size: 14px;
+    height: 12px;
+    margin-top: 12px;
+    letter-spacing: 1px;
+    text-decoration: none;
+    position: relative;
+  }
+
+  &-link:hover::before {
+    content: "";
+    position: absolute;
+    right: 0;
+    bottom: -10px;
+    width: 46%;
+    height: 2px;
+    background-color: $color-yellow-light;
+  }
+
+  &-link:hover::after {
+    content: "";
+    position: absolute;
+    left: 0;
+    bottom: -10px;
+    width: 46%;
+    height: 2px;
+    background-color: $color-yellow-light;
+  }
+
+  &-link:hover {
+    text-decoration: none;
+    color: $color-yellow-main;
+  }
+}
+
+.main {
+  display: flex;
+  justify-content: center;
+  margin: 20px;
+  height: calc(100vh - (115px + 50px));
+  min-height: 370px;
+
+  &-window {
+    padding: 10px;
+    margin: 5px;
+    width: 525px;
+    height: 100%;
+    min-height: 350px;
+    border: 2px solid $color-yellow-main;
+    border-radius: 10px;
+
+    &__title {
+      color: $color-yellow-light;
+      text-transform: uppercase;
+    }
+
+    &__devider {
+      border: 0;
+      height: 2px;
+      background: $color-yellow-main;
+    }
+  }
+}
+
+.footer {
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  height: 48px;
+  background-color: $color-gray;
+  box-shadow: 0px -5px 10px rgba(0, 0, 0, 0.3);
+
+  &-link {
+    color: $color-white;
+    font-size: 14px;
+    height: 12px;
+    margin-top: 15px;
+    letter-spacing: 1px;
+    text-decoration: none;
+    position: relative;
+  }
+
+  &-link:hover::before {
+    content: "";
+    position: absolute;
+    right: 0;
+    bottom: -10px;
+    width: 46%;
+    height: 2px;
+    background-color: $color-yellow-light;
+  }
+
+  &-link:hover::after {
+    content: "";
+    position: absolute;
+    left: 0;
+    bottom: -10px;
+    width: 46%;
+    height: 2px;
+    background-color: $color-yellow-light;
+  }
+
+  &-link:hover {
+    text-decoration: none;
+    color: $color-yellow-main;
+  }
+}
+
+.__capital {
+  text-transform: uppercase;
 }
 </style>
